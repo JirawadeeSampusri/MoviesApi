f0f05eb3af05f994a4a0d169d7e8134772028899
Jirawadee Sampusri <jirawadeelovely@hotmail.com>
2020-12-19 23:45:16 +0700

    remove unused directory

--- autogen/openapi_server/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from openapi_server.models.actor import Actor
-from openapi_server.models.average_each_year import AverageEachYear
-from openapi_server.models.director import Director
-from openapi_server.models.movie import Movie
